1d9f565c2517d1a354c7b6c9d5ec252e707ebd3a
msimonin <matthieu.simonin@inria.fr>
2017-05-05 14:52:27 +0200

    use raw result in plot instead of normalized one

--- bar_chart.py
+++ bar_chart.py
@@ -14,40 +14,40 @@ def draw(rpc_call, rpc_cast, latency, filename):
     bar_width = 0.35
     opacity = 0.8
 
-    rects1 = plt.bar(index, rpc_call_norm, bar_width,
+    rects1 = plt.bar(index, rpc_call, bar_width,
                      alpha=opacity,
                      color = '#ff3c00',
                      label='RPC call')
 
-    rects2 = plt.bar(index + bar_width, rpc_cast_norm, bar_width,
+    rects2 = plt.bar(index + bar_width, rpc_cast, bar_width,
                      alpha=opacity,
                      color='#ff997a',
                      label='RPC cast')
 
-    plt.xlabel('Latency')
-    plt.ylabel('msg/s (normalized)')
+    plt.xlabel('')
+    plt.ylabel('msg/s')
     plt.title('Latency impact on RPCs')
     plt.xticks(index + bar_width, latency)
-    plt.legend()
+    plt.legend(loc=7)
 
-#plt.tight_layout()
+    plt.tight_layout()
 #plt.show()
     plt.savefig(filename, facecolor='white', transparent=True)
 
 rpc_call = [218., 62., 16.]
 mcall = max(rpc_call)
-rpc_call_norm = [x/mcall * 100  for x in rpc_call]
+#rpc_call = [x/mcall * 100  for x in rpc_call]
 rpc_cast = [1062., 84., 18.]
 mcast = max(rpc_cast)
-rpc_cast_norm = [x/mcast * 100 for x in rpc_cast]
+#rpc_cast = [x/mcast * 100 for x in rpc_cast]
 
-draw(rpc_call_norm, rpc_cast_norm, latency, 'far_client.png')
+draw(rpc_call, rpc_cast, latency, 'far_client.png')
 
 rpc_call = [215., 37., 14.]
 mcall = max(rpc_call)
-rpc_call_norm = [x/mcall * 100  for x in rpc_call]
+#rpc_call = [x/mcall * 100  for x in rpc_call]
 rpc_cast = [1129., 1029., 1170.]
 mcast = max(rpc_cast)
-rpc_cast_norm = [x/mcast * 100 for x in rpc_cast]
+#rpc_cast = [x/mcast * 100 for x in rpc_cast]
 
-draw(rpc_call_norm, rpc_cast_norm, latency, 'far_server.png')
+draw(rpc_call, rpc_cast, latency, 'far_server.png')
